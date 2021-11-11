57b59423191fb6c4da2b88cf7c9aa47de1314a2b
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-11-11 22:01:44 +0200

    search from added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from './Header/Header';
+import { SearchForm } from './SearchForm/SearchForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchForm />
     </div>
   );
 }
---
+++ src/SearchForm/SearchForm.js
@@ -0,0 +1,31 @@
+import 'SearchForm/SearchForm.css';
+import { Component } from 'react';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <form class="search-form" id="search-form">
+        <input
+          class="search-form__input"
+          type="text"
+          name="query"
+          autocomplete="off"
+          placeholder="Search images..."
+        />
+        <button class="btn search-form__btn" type="submit">
+          Search
+        </button>
+        <button
+          class="btn search-form__btn"
+          type="button"
+          data-action="reset"
+          disabled
+        >
+          Reset
+        </button>
+      </form>
+    );
+  }
+}
+
+export { SearchForm };

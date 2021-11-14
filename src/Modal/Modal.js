b4521c101e7cc491a04a9202f8ef452b807bc6c7
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-11-14 15:39:09 +0200

    feature modal close
    closing modal window by escape button clock and backdrop click with removing event listeners on closing

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,11 +1,35 @@
 import './Modal.css';
+
+import { createPortal } from 'react-dom';
 import { Component } from 'react';
 
+const modalRoot = document.querySelector('#modal-root');
+
 class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.onEscapePress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onEscapePress);
+  }
+
+  onEscapePress = e => {
+    if (e.code !== 'Escape') {
+      return;
+    } else {
+      this.props.onModalClose();
+    }
+  };
+
+  onBackdropClick = e => {
+    return e.target === e.currentTarget ? this.props.onModalClose() : null;
+  };
+
   render() {
-    return (
+    return createPortal(
       <div className="lightbox js-lightbox">
-        <div className="lightbox__overlay"></div>
+        <div className="lightbox__overlay" onClick={this.onBackdropClick}></div>
         <div className="lightbox__content">
           <img
             className="lightbox__image"
@@ -40,7 +64,8 @@ class Modal extends Component {
               </div> */}
           </div>
         </div>
-      </div>
+      </div>,
+      modalRoot,
     );
   }
 }
